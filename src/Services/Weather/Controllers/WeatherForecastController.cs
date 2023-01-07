d52584ff55769ae19181db3b6d621d27ab7b77d2
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-01-07 11:53:40 +0200

    #5 | Init ApiGateaway

--- src/Services/Weather/Controllers/WeatherForecastController.cs
+++ src/Services/Weather/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Weather.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/weather/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
