9ef86d7f8e9e9524965b8515cb887aeab382e48f
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-02-16 19:05:58 +0200

    #45 | Add temp cors policy

---
+++ src/BuildingBlocks/Web/CustomCorsExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuildingBlocks.Web;
+
+public static class CustomCorsExtensions
+{
+    private const string CorsPolicy = "corsPolicy";
+
+    public static IServiceCollection AddCustomCors(this IServiceCollection services)
+    {
+        services.AddCors(opt =>
+        {
+            opt.AddPolicy(CorsPolicy, builder =>
+            {
+                builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+        });
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseCustomCors(this IApplicationBuilder app)
+    {
+        app.UseCors(CorsPolicy);
+        return app;
+    }
+}
