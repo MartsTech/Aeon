c43b98a0d6e7c725fa3ab6619dfe186c0e05b654
SavinaMih <94933624+SavinaMih@users.noreply.github.com>
2023-02-21 20:33:59 +0200

    Models of CartService

---
+++ Cart.Domain/Models/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cart.Domain.Models
+{
+    public sealed class CartItem
+    {
+        
+          public Product Product { get; set; }
+
+           public int Quantity { get; set; }
+      
+    }
+}
---
+++ Cart.Domain/Models/CustomerCart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cart.Domain.Models
+{
+    public sealed class CustomerCart
+    {
+        public Guid Id { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public List<CartItem> Items { get; set; } = new List<CartItem>();
+
+        public int TotalItems { get; set; }
+
+        public decimal Subtotal { get; set; }
+    }
+}
