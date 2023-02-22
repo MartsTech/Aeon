8ded01051e1115f377094241cac3167b766048ec
SavinaMih <94933624+SavinaMih@users.noreply.github.com>
2023-02-22 21:14:37 +0200

    add entity factory

--- OrderService.Persistence/EntityFactory.cs
+++ OrderService.Persistence/EntityFactory.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace OrderService.Persistence
 {
-    internal class EntityFactory
+    public class EntityFactory : IEntityFactory
     {
+       
+
+        public Order NewOrder(Guid productId, int productQuantity, Guid listId, Guid userId)
+        {
+            return new Order(new Guid(), productId, productQuantity, DateOnly.FromDateTime(DateTime.Now), userId, listId);
+        }
+
+        public Order NewOrderWithExistingId(Guid id, Guid productId, int productQuantity, Guid listId, Guid userId)
+        {
+            return new Order(id, productId, productQuantity, DateOnly.FromDateTime(DateTime.Now), listId, userId);
+        }
     }
 }
