738827ed19e78d87e0433e2f6eb4288e91837ff4
SavinaMih <94933624+SavinaMih@users.noreply.github.com>
2023-02-22 20:25:45 +0200

    add order input

--- OrderService.Application/AddOrder/AddOrderInput.cs
+++ OrderService.Application/AddOrder/AddOrderInput.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace OrderService.Application
 {
-    internal class AddOrderInput
+    public class AddOrderInput
     {
+        [Required] public Guid ProductId { get; set; }
+        [Required] public int Quantity { get; set; }
+        [Required] public Guid ListId { get; set; }
     }
 }
--- OrderService.Application/AddOrder/AddOrderInputValidator.cs
+++ OrderService.Application/AddOrder/AddOrderInputValidator.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
+
 
 namespace OrderService.Application.AddOrder
 {
-    internal class AddOrderInputValidator
+    nternal class AddOrderInputValidator : AbstractValidator<AddOrderInput>
     {
+        public AddOrderInputValidator()
+        {
+            RuleFor(x => x.ProductId).NotEmpty();
+            RuleFor(x => x.ListId).NotEmpty();
+            RuleFor(x => x.Quantity).NotEmpty().GreaterThanOrEqualTo(1);
+        }
     }
 }
