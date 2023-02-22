28f42370fa97c9bc4849d37100622d64bcb12960
SavinaMih <94933624+SavinaMih@users.noreply.github.com>
2023-02-22 20:46:54 +0200

    order dto

--- OrderService.Application/OrderDto.cs
+++ OrderService.Application/OrderDto.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OrderService.Domain.Orders;
+
+namespace OrderService.Application.Orders
+{
+    public class OrderDto : IOrder
+    {
+        public OrderDto(IOrder order)
+        {
+            Id = order.Id;
+            ProductId = order.ProductId;
+            ProductQuantity = order.ProductQuantity;
+            DateAdded = order.DateAdded;
+            ListId = order.ListId;
+            UserId = order.UserId;
+        }
+
+        public Guid Id { get; }
+        public Guid ProductId { get; }
+        public int ProductQuantity { get; }
+        public DateOnly DateAdded { get; }
+        public Guid ListId { get; }
+
+        public Guid UserId { get; }
+    }
+}
