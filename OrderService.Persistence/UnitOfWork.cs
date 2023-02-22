02c7c3dd06c8153d2944bd4a3040942aedcca749
SavinaMih <94933624+SavinaMih@users.noreply.github.com>
2023-02-22 21:18:12 +0200

    update persistence

--- OrderService.Persistence/UnitOfWork.cs
+++ OrderService.Persistence/UnitOfWork.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace OrderService.Persistence
 {
-    internal class UnitOfWork
+    public sealed class UnitOfWork : IUnitOfWork, IDisposable
     {
+        private readonly OrderServiceDbContext _context;
+        private bool _disposed;
+
+        public UnitOfWork(OrderServiceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var affectedRows = await _context
+                .SaveChangesAsync(cancellationToken)
+                .ConfigureAwait(false);
+
+            return affectedRows;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposed && disposing)
+            {
+                _context.Dispose();
+            }
+
+            _disposed = true;
+        }
     }
 }
