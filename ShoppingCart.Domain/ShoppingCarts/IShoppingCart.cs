5e25730d90d41e7b318f7765cec2bca0c919e33c
SavinaMih <94933624+SavinaMih@users.noreply.github.com>
2023-02-25 14:23:13 +0200

    update shopping cart's domain

---
+++ ShoppingCart.Domain/ShoppingCarts/IShoppingCart.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookmarks.Domain.Wishlists
+{
+    public interface IShoppingCart
+    {
+        public Guid Id { get; }
+        public Guid UserId { get; }
+        public DateOnly DateCreated { get; }
+    }
+}
---
+++ ShoppingCart.Domain/ShoppingCarts/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿using ShoppingCart.Domain.ShoppingCarts;
+
+namespace ShoppingCart.Domain.ShoppingCarts
+{
+    public class ShoppingCart : IShoppingCart
+    {
+        public ShoppingCart(Guid id, Guid userId, DateOnly dateCreated)
+        {
+            Id = id;
+            UserId = userId;
+            DateCreated = dateCreated;
+            Bookmarks = new List<ShoppingCart>();
+        }
+
+        public Guid Id { get; }
+        public Guid UserId { get; }
+        public DateOnly DateCreated { get; }
+        public IList<ShoppingCart> ShoppingCarts { get; }
+    }
+}
