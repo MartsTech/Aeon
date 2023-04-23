a36ecc593250c53591df9080acf639f86824cbc2
Savina Mihayova <94933624+SavinaMih@users.noreply.github.com>
2023-04-23 08:51:00 +0300

    add changes

---
+++ src/Services/OrderService/OrderService.Domain/IEntityFactory.cs
@@ -0,0 +1,13 @@
+﻿using OrderService.Domain.Orders;
+using OrderService.Domain.OrderLists;
+
+namespace OrderService.Domain
+{
+    public interface IEntityFactory
+    {
+        OrderList NewList(Guid userId);
+
+        Order NewOrder(Guid productId, int productQuantity, Guid listId, Guid userId);
+
+    }
+}
--- src/Services/OrderService/OrderService.Persistence/OrdersDbContext.cs
+++ src/Services/OrderService/OrderService.Persistence/OrdersDbContext.cs
@@ -3,22 +3,23 @@ using Catalog.Domain.Categories;
 using Catalog.Domain.Comments;
 using Catalog.Domain.Products;
 using OrderService.Domain.Orders;
-using OrderService.Persistence.Orders;
+using OrderService.Domain.OrderLists;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Reflection.Emit;
+using Catalog.Persistence.Products;
 
 namespace OrderService.Persistence;
 
-public sealed class CatalogDbContext : DbContextBase
+public sealed class OrdersDbContext : DbContextBase
 {
-    public CatalogDbContext(DbContextOptions options) : base(options)
+    public OrdersDbContext(DbContextOptions options) : base(options)
     {
     }
 
     public DbSet<Product> Products { get; set; }
     public DbSet<Order> Orders { get; set; }
-    public DbSet<OrderLists> OrderLists { get; set; }
+    public DbSet<OrderList> OrderLists { get; set; }
    
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
